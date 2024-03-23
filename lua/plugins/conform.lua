56f386a09a4d4124a488175c4e9f74b48920e33f
saytaM12 <matyas.oujezdsky@gmail.com>
2024-03-23 14:49:42 +0100

    add clang_format to h and hpp files
    I don't know if this isn't implied with c and cpp, but it doesn't hurt

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,14 +1,16 @@
 return {
-    "stevearc/conform.nvim",
-    opts = {
-        formatters_by_ft = {
-            c = { "clang_format" },
-            cpp = { "clang_format" },
-        },
-        formatters = {
-            clang_format = {
-                args = { "--style=file:/home/sayta/.config/nvim/.clang-format" },
-            },
-        },
-    }
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      c = { "clang_format" },
+      cpp = { "clang_format" },
+      h = { "clang_format" },
+      hpp = { "clang_format" },
+    },
+    formatters = {
+      clang_format = {
+        args = { "--style=file:/home/sayta/.config/nvim/.clang-format" },
+      },
+    },
+  },
 }
